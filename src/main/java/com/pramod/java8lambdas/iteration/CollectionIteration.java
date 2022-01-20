a3ce9b28e5111f1f23e90f68084d9c94cdef04ff
Pramod <sunkad.pramod@gmail.com>
2022-01-20 12:21:34 +0530

    Iteration collection

--- src/main/java/com/pramod/java8lambdas/iteration/CollectionIteration.java
+++ src/main/java/com/pramod/java8lambdas/iteration/CollectionIteration.java
@@ -1,7 +1,9 @@
 package com.pramod.java8lambdas.iteration;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.pramod.lambda8.Person;
 
@@ -17,6 +19,28 @@ public class CollectionIteration{
 				new Person(45, "Charlotte", "Bronte"),
 				new Person(39, "Mathew", "Arnold")
 			);
+		// Java 7
+			// method 1 (indexed FOR loop):
+			for (int i = 0; i < people.size(); i++) {
+				System.out.println(people.get(i));
+			}
+			// method 2: (forEach Loop)
+			for (Person p : people) {
+				System.out.println(p.toString());
+			}
+		// Java 8
+			System.out.println("JAVA 8");
+			people.forEach(System.out::println);
+			
+		Map<String, Integer> map = new HashMap<>();
+		map.put("Pramod", 5);
+		map.put("Praveen", 3);
+		map.put("Pradeep", 1);
+		map.put("Vinayak", 10);
+		map.put("Raj", 19);
+		
+		// Iterate over map using Java8 Lambdas		
+		map.forEach((k, v) -> System.out.println(k + " : " + v));
 		
 	}
 
